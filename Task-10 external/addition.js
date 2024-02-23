bd167878ff3e5a335c208804e9692b6ed66efa7a
DeshineniKavya <158246371+DeshineniKavya@users.noreply.github.com>
2024-02-23 16:44:46 +0530

    Add files via upload

---
+++ Task-10 external/addition.js
@@ -0,0 +1,5 @@
+function addition(a,b){
+    var result
+    result = a+b
+    console.log(result)
+    }
---
+++ Task-10 external/average.js
@@ -0,0 +1,5 @@
+function average(a,b,c){
+    var result
+    result=(a+b+c)/3
+    console.log(result)
+}
